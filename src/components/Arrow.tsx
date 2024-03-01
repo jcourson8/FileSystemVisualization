767425754dc4a3525ed190d39f345df5aa70762c
James Courson <80439017+jcourson8@users.noreply.github.com>
2024-03-01 12:41:44 -0600

    changed colors

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -3,7 +3,7 @@ export const Arrow = () => {
     return (
         <div>
         <svg width="15" height="15" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
-        <path d="M10 5 L30 20 L10 35" stroke="black" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M10 5 L30 20 L10 35" stroke="gray" stroke-width="4" fill="none" stroke-linecap="round" stroke-linejoin="round"/>
         </svg>
         </div>
     );
